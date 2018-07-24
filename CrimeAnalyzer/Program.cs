be7600043bda7ede216bcccc53dd67868fee2af4
Algernon132 <40308777+Algernon132@users.noreply.github.com>
2018-07-24 16:18:48 -0500

    check for command line usage

--- CrimeAnalyzer/Program.cs
+++ CrimeAnalyzer/Program.cs
@@ -10,6 +10,25 @@ namespace CrimeAnalyzer
     {
         static void Main(string[] args)
         {
+            if(args==null || args.Length != 2)
+            {
+                Console.WriteLine("Incorrect arguments. Usage should be \"CrimeAnalyzer.exe <crime_csv_file_path> <report_file_path>\". \n");
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+
+
+        }
+
+
+        static void OpenCSB(string filename)
+        {
+
+        }
+
+        static void ReadCSV(string filename)
+        {
+
         }
     }
 }
